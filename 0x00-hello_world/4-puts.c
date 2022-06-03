68e68c6b8b8096574cd06b4155b49f02647b588f
Madcyb <trabelsixtaha@gmail.com>
2022-06-02 18:20:56 -0700

    Project 0x00-Hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * Description: prints "Programming is like building a multilingual puzzle"
+ * main: Entry point
+ * prints "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }

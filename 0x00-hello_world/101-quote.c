85ec354f66e6849dcd27495b8152ca5a514ab76f
Madcyb <trabelsixtaha@gmail.com>
2022-06-02 19:19:09 -0700

    optional tasks for 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 
 /**
  * main - print quote
- * Description: prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * Description: prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, 
+ * followed by a new line, to the standard error.
  * Return: Always 1 (Success)
  */
 

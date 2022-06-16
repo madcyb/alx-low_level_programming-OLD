f76029c785bb5f5524bf5e1c98ca8a3bea422e3b
Madcyb <trabelsixtaha@gmail.com>
2022-06-16 12:19:46 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase and in uppercase 
+ * main - print the letters of the alphabet
+ * Description: print the letters of the alphabet except e, q
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-int ch;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
-putchar('\n');
+int main(void)
+{ int i = 97;
+
+while (i < 123)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+i++;
+}
+putchar(10);
+
 return (0);
 }
